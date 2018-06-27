7d02570549d07d487e88f0bcd3ddfed10c5e5cd4
Michael Diego <itsmichaeldiego@gmail.com>
2018-06-27 20:37:41 +0300

    Longer description

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Michael Diego&apos;s website' }
+      { hid: 'description', name: 'description', content: 'Michael Diego is an enthusiastic Frontend Engineer, working on digital projects all over the world.' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
