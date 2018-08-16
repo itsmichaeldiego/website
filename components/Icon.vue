73f865f7f4a612045508f42569ca66b0fe7a43eb
Michael Diego <itsmichaeldiego@gmail.com>
2018-08-16 16:44:04 +0200

    Add icons to the mix

---
+++ components/Icon.vue
@@ -0,0 +1,11 @@
+<template>
+  <span
+    v-bind:class="`icon-${type}`"
+  ></span>
+</template>
+
+<script>
+  export default {
+    props: ['type'],
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,18 @@
   <section>
     <fixed-header />
     <banner />
+    <icon type="github" />
   </section>
 </template>
 
 <script>
+  import Icon from '~/components/Icon.vue'
   import Banner from '~/components/Banner.vue'
   import FixedHeader from '~/components/FixedHeader.vue'
 
   export default {
     components: {
+      Icon,
       Banner,
       FixedHeader,
     }
