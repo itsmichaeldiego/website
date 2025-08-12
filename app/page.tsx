b1aaf1ce943cea4faed662686038f0192dcbb416
itsmichaeldiego <itsmichaeldiego@gmail.com>
2025-08-12 20:54:44 +0200

    Rename resumee to itsmichaeldiego-resumee.pdf

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <div className="mt-6 flex flex-wrap items-center justify-center gap-3">
           <Link
             download
-            href="/resumee.pdf"
+            href="/itsmichaeldiego-resumee.pdf"
             className="px-4 py-2 rounded-xl bg-primary text-primary-foreground hover:opacity-90 transition"
           >
             Download CV
